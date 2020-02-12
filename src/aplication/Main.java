a9c546bee737c79fd5f2c28f0f365cc642ba9685
gabrielMaia-1 <gab.maia@outlook.com>
2020-02-11 22:10:55 -0400

    added Board and Piece

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -1,9 +1,11 @@
 package aplication;
 
+import boardgame.Board;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Board boar = new Board(8,8);
 
 	}
 
